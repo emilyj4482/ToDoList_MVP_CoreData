9b4dbbda7985e85968fa4b5f97cbe261babee65c
emilyj4482 <emilyj4482@gmail.com>
2023-10-14 02:43:42 +0900

    test commit from macbook

--- ToDoList_MVP_FireBase/Scenes/Main/MainListViewController.swift
+++ ToDoList_MVP_FireBase/Scenes/Main/MainListViewController.swift
@@ -14,8 +14,6 @@ final class MainListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.viewDidLoad()
-        
-        // test comment
     }
 }
 
