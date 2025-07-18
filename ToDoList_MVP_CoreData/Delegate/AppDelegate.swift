80ad12e28a5a75d53bd9868941142d4bbe283cb7
emilyj4482 <emilyj4482@gmail.com>
2025-07-18 20:24:46 +0900

    [chore] project replaced

--- ToDoList_MVP_CoreData/Delegate/AppDelegate.swift
+++ ToDoList_MVP_CoreData/Delegate/AppDelegate.swift
@@ -1,24 +1,24 @@
 //
 //  AppDelegate.swift
-//  ToDoList_MVP_FireBase
+//  ToDoList_MVP_CoreData
 //
 //  Created by EMILY on 2023/10/14.
 //
 
 import UIKit
-import FirebaseCore
 
-@UIApplicationMain
+@main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
         return true
     }
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
     }
 }
---
+++ ToDoList_MVP_CoreData/Scene/Main/MainListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainListViewController.swift
+//  ToDoList_MVP_CoreData
+//
+//  Created by EMILY on 2023/10/14.
+//
+
+import UIKit
+
+class MainListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+#Preview {
+    MainListViewController()
+}
